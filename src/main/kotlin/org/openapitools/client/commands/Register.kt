affa8b6c11e72c57604e757b3e953b161c8c2371
DevCSSisHard <34226427+DevCSSisHard@users.noreply.github.com>
2023-07-06 10:59:32 -0700

    Removed the idea of Registering a client as adding in a token via code is probably a really bad idea even though it would make it easier when the API resets weekly...

--- src/main/kotlin/org/openapitools/client/commands/Register.kt
+++
@@ -1,9 +0,0 @@
-package org.openapitools.client.commands
-
-class Register: Command {
-    override val commandName: String = "Register"
-
-    override suspend fun processCommand(remainingCommands: List<String>) {
-        //Place holder if I do decide to implement a registration.
-    }
-}
